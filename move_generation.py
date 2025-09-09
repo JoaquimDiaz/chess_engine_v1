83a45f167264f429e1b0fb03936471cf089e2fdf
Joaquim Diaz <joaquimdiaz11@gmail.com>
2025-09-09 20:03:09 +0200

    [UPD] generate_all_moves

--- move_generation.py
+++ move_generation.py
@@ -373,47 +373,9 @@ if __name__ == "__main__":
     B_moves_a1 = generate_king_move(board, 'a1')
 
     print(f"'a1' -> {B_moves_a1}")
+    board = cb.create_starting_position()
 
     a = find_pieces(board, 'w')
-    #a = generate_all_moves(board, 'b')
     print(a)
-
-    '''
-    board[square_to_index('h4')] = -piece
-    board[square_to_index('a4')] = -piece
-    board[square_to_index('e1')] = -piece
-    board[square_to_index('e8')] = -piece
-
-    R_moves_e4 = generate_rook_move(board, 'e4')
-    
-    print(f"'e4' -> {R_moves_e4}")
-
-    board[square_to_index('e4')] = 2
-    N_moves_e4 = generate_knight_move(board, 'e4')
-    N_moves_b1 = generate_knight_move(board, 'b1')
-    N_moves_g1 = generate_knight_move(board, 'g1')
-    N_moves_g8 = generate_knight_move(board, 'g8')
-    
-    print(f"'b1' -> {N_moves_b1}")
-    print(f"'g1' -> {N_moves_g1}")
-    print(f"'g8' -> {N_moves_g8}")
-    print(f"'e4' -> {N_moves_e4}")
-
-    board[square_to_index('g6')] = 1
-    board[square_to_index('f3')] = -1
-    
-    pawn_moves_a2 = generate_pawn_move(board=board, square='a2')
-    pawn_moves_e2 = generate_pawn_move(board=board, square='e2')
-    pawn_moves_a7 = generate_pawn_move(board=board, square='a7')
-    pawn_moves_f7 = generate_pawn_move(board=board, square='f7')
-    pawn_moves_e1 = generate_pawn_move(board=board, square='e1')
-    pawn_moves_e4 = generate_pawn_move(board=board, square='e4')
-
-    print(f"'a2' -> {pawn_moves_a2}")
-    print(f"'e2' -> {pawn_moves_e2}")
-    print(f"'a7' -> {pawn_moves_a7}")
-    print(f"'f7' -> {pawn_moves_f7}")
-    print(f"'e1' -> {pawn_moves_e1}")
-    print(f"'e4' -> {pawn_moves_e4}")
-       ''' 
-        
+    a = generate_all_moves(board, 'w')
+    print(a)
