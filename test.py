a523f96acd3aea8195d728d50150ef0ccb3b175e
Joaquim Diaz <joaquimdiaz11@gmail.com>
2025-09-16 10:02:18 +0200

    [UPD] untrack test file

--- test.py
+++
@@ -1,9 +0,0 @@
-import chess_board as cb
-import config
-
-board = cb.create_empty_board()
-
-board[cb.square_to_index("a2")] = config.BLACK_BISHOP
-board[cb.square_to_index("d5")] = config.WHITE_KING
-
-cb.display_board(board)
