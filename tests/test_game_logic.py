64ff5ad6b091790da9b268b244c39bc0de9cbf51
Joaquim Diaz <joaquimdiaz11@gmail.com>
2025-09-23 21:10:31 +0200

    [UPD] adding ChessBoard logic to gl tests

--- tests/test_game_logic.py
+++ tests/test_game_logic.py
@@ -1,104 +1,135 @@
 import chess_board as cb
+from chess_board import ChessBoard, parse_index
 import move_generation as mv
 import game_logic as gl
 import config
+from config import (
+    Piece,
+    PieceMoves,
+    CastlingState,
+    WHITE,
+    BLACK,
+    EMPTY_SQUARE,
+    PAWN,
+    KNIGHT,
+    BISHOP,
+    ROOK,
+    QUEEN,
+    KING,
+    WHITE_PAWN,
+    WHITE_KNIGHT,
+    WHITE_BISHOP,
+    WHITE_ROOK,
+    WHITE_QUEEN,
+    WHITE_KING,
+    BLACK_PAWN,
+    BLACK_KNIGHT,
+    BLACK_BISHOP,
+    BLACK_ROOK,
+    BLACK_QUEEN,
+    BLACK_KING,
+    PinnedPiece,
+)
 import pytest
 
-test_board = cb.create_empty_board()
 
-test_board[0] = config.BLACK_BISHOP
-test_board[62] = config.BLACK_ROOK
-test_board[6] = config.BLACK_ROOK
-test_board[54] = config.WHITE_KING
-test_board[5] = config.BLACK_KING
+def sq(square: str) -> int:
+    """Quick helper for tests: sq('a1') return index 0"""
+    return cb.square_to_index(square)
+
+
+b = ChessBoard()
+
+b[0] = BLACK_BISHOP
+b[62] = BLACK_ROOK
+b[6] = BLACK_ROOK
+b[54] = WHITE_KING
+b[5] = BLACK_KING
 
 
 def test_diagonal_checks():
-    board = test_board[:]
-    board_state = cb.find_pieces(board)
+    file, rank = cb.parse_index(b.w_king_idx)
     checking_piece, pinned_piece = gl.directional_check(
-        board, board_state[4], config.WHITE, (-1, -1)
+        b.board, file, rank, WHITE, (-1, -1)
     )
     assert pinned_piece is None
     assert checking_piece is not None
-    assert board[checking_piece[1]] == config.BLACK_BISHOP
+    assert checking_piece == Piece(BLACK_BISHOP, sq("a1"))
 
 
 def test_diagonal_pin():
-    board = test_board[:]
-    board[18] = config.WHITE_QUEEN
-    board_state = cb.find_pieces(board)
-
+    b2 = b.copy()
+    b2["c3"] = WHITE_QUEEN
+    file, rank = cb.parse_index(b2.w_king_idx)
     checking_piece, pinned_piece = gl.directional_check(
-        board, board_state[4], config.WHITE, (-1, -1)
+        b2.board, file, rank, WHITE, (-1, -1)
     )
 
     assert checking_piece is None
     assert pinned_piece is not None
-    assert board[pinned_piece[1]] == config.WHITE_QUEEN
+    assert pinned_piece.piece == Piece(WHITE_QUEEN, sq("c3"))
 
 
 def test_aligned_checks():
-    board = test_board[:]
-    board_state = cb.find_pieces(board)
+    b3 = b.copy()
+
+    file, rank = cb.parse_index(b3.w_king_idx)
     checking_piece, pinned_piece = gl.directional_check(
-        board, board_state[4], config.WHITE, (0, 1)
+        b3.board, file, rank, WHITE, (0, 1)
     )
     assert pinned_piece is None
     assert checking_piece is not None
-    assert board[checking_piece[1]] == config.BLACK_ROOK
+    assert checking_piece == Piece(BLACK_ROOK, sq("g8"))
 
     checking_piece, pinned_piece = gl.directional_check(
-        board, board_state[4], config.WHITE, (0, -1)
+        b3.board, file, rank, WHITE, (0, -1)
     )
     assert pinned_piece is None
     assert checking_piece is not None
-    assert board[checking_piece[1]] == config.BLACK_ROOK
+    assert checking_piece == Piece(BLACK_ROOK, sq("g1"))
 
 
 def test_aligned_pin():
-    board = test_board[:]
-    board[14] = config.WHITE_QUEEN
-    board_state = cb.find_pieces(board)
+    b4 = b.copy()
+    b4["g2"] = WHITE_QUEEN
 
+    file, rank = cb.parse_index(b4.w_king_idx)
     checking_piece, pinned_piece = gl.directional_check(
-        board, board_state[4], config.WHITE, (0, -1)
+        b4.board, file, rank, WHITE, (0, -1)
     )
 
     assert checking_piece is None
     assert pinned_piece is not None
-    assert board[pinned_piece[1]] == config.WHITE_QUEEN
+    assert pinned_piece.piece == Piece(WHITE_QUEEN, sq("g2"))
 
 
 def test_knight_check():
-    board = cb.create_empty_board()
-    board[2] = config.BLACK_KING
-    board[8] = config.WHITE_KNIGHT
-    board[63] = config.WHITE_KING
-    board_state = cb.find_pieces(board)
+    b = ChessBoard()
+    b["c1"] = BLACK_KING
+    b["a2"] = WHITE_KNIGHT
 
-    result = gl.knight_check(board, config.BLACK, board_state[5])
+    file, rank = cb.parse_index(b.b_king_idx)
+    result = gl.knight_check(b.board, file, rank, BLACK)
 
     assert result is not None
-    assert result[0] == 2
-    assert result[1] == 8
+    assert result == Piece(WHITE_KNIGHT, sq("a2"))
 
 
 def test_analyze_king_safety():
-    b = cb.create_empty_board()
+    b = ChessBoard()
     with pytest.raises(ValueError):
-        gl.analyze_king_safety(b, 0, config.BLACK)
+        gl.analyze_king_safety(b.board, sq("a1"), BLACK)
 
-    b[0] = config.WHITE_KING
-    b[7] = config.BLACK_KING
-    b[63] = config.BLACK_BISHOP
+    b["a1"] = WHITE_KING
+    b["a8"] = BLACK_KING
+    b["h8"] = BLACK_BISHOP
 
-    checking_pieces, pinned_pieces = gl.analyze_king_safety(b, 0, config.WHITE)
+    checking_pieces, pinned_pieces = gl.analyze_king_safety(
+        b.board, b.w_king_idx, WHITE
+    )
 
     assert pinned_pieces == []
     assert checking_pieces != []
     assert len(checking_pieces) == 1
 
-    piece, idx = checking_pieces[0]
-    assert piece == config.BLACK_BISHOP
-    assert idx == 63
+    assert checking_pieces[0] == Piece(BLACK_BISHOP, sq("h8"))
